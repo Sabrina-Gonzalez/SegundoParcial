039ed334dac4cf57a3abbeb982bb0be79954588e
Sabrina-Gonzalez <sabrina.gonzalez@est.fi.uncoma.edu.ar>
2025-05-30 01:29:14 -0300

    agregado

---
+++ especializacion2.php
@@ -0,0 +1,35 @@
+<?php
+class G2 extends G{
+    private $a;
+    private $b;
+
+    public function __construct($a,$b)
+    {
+        parent::__construct();
+        $this->a=$a;
+        $this->b=$b;
+    }
+
+    //Getters
+    public function getA(){
+        return $this->a;
+    }
+    public function getB(){
+        return $this->b;
+    }
+
+    //Setters
+    public function setA($a){
+        $this->a=$a;
+    }
+    public function setCoefPenalizacion($b){
+        $this->b=$b;
+    }
+
+    //toString
+    public function __toString()
+    {
+        return parent::__toString().
+        "I";
+    }
+}
---
+++ generalizacion.php
@@ -0,0 +1,54 @@
+<?php
+class G{
+    private $a;
+    private $b;
+    private $c;
+    private $d;
+    private $e;
+
+    //Constructor
+    public function __construct()
+    {
+        
+    }
+
+    //Getters
+    public function getA(){
+        return ;
+    }
+    public function getb(){
+        return ;
+    }
+    public function getc(){
+        return ;
+    }
+    public function getd(){
+        return ;
+    }
+    public function gete(){
+        return ;
+    }
+
+    //Setters
+    public function setA($a){
+        $this->a;
+    }
+    public function setb($b){
+        $this->b=$b;
+    }
+    public function setc($c){
+        $this->c=$c;
+    }
+    public function setd($d){
+        $this->d=$d;
+    }
+    public function sete($e){
+        $this->e=$e;
+    }
+
+    //toString
+    public function __toString()
+    {
+        return "s";
+    }
+}
